de5c14a092d9a8ef6e6440e9fb3b8b07f3ef9c4e
Lukas Jurk <lukas.jurk@tu-dresden.de>
2020-07-06 17:56:45 +0200

    add eddis script for his midi2cv

--- src/main.cpp
+++ src/main.cpp
@@ -1,264 +1,175 @@
-/*  https://www.instructables.com/id/Another-MIDI-to-CV-Box-/
- *  Another MIDI2CV Box
- *  MIDI interface for Hz/V and V/oct synths. Connect your favourite keyboard or DAW to the box via MIDI;
- *  the box will in turn control your synth gate On/Off (straight +5V/0V or S-Trig, reversed,
- *  0V/+5V), pitch (variable voltage via DAC output), velocity (0V to +5V, RC filtered) and a MIDI Control
- *  Change variable at your will (0V to +5V, RC filtered).
- *
- *  Connections:
- * (1) MIDI connector: see general online reference
- * (2) DAC MCP4725:
- * SDA pin to A4/SDA (Arduino UNO adn nano)
- * SCL pin to A5/SCL (Arduino UNO adn nano)
- * GND to GND
- * VCC to +5V
- * (3) Outputs:
- * Arduino gate OUT pin (pin 12 by default) to synth gate/trigger IN via 1K Ohm resistor
- * Arduino velocity OUT pin (pin 10 by default) to synth VCA IN via RC filter (1K Ohm, 100uF)
- * Arduino MIDI CC OUT pin (pin 9 by default) to synth VCF IN via RC filter (1K Ohm, 100uF)
- *
- * Gate Pin for Channel 2:
- * Note - Pin
- * 32 - 2
- * 33 - 4
- * 34 - 7
- * 34 - 8
- *
- * Gate Pin for Channel 1: 12
- * reacts to every Note
- *
- * Arduino PWM-Pins: 3, 5, 6, 9, 10, 11
- * Arduino Gate-Pins: 2, 4, 7, 8, 12
- * DAC OUT to synth VCO IN
- *
- * MIDI messages table:
- *    Message                      Status    Data 1               Data 2
- *    Note Off                     8n        Note Number          Velocity
- *    Note On                      9n        Note Number          Velocity
- *    Polyphonic Aftertouch        An        Note Number          Pressure
- *    Control Change               Bn        Controller Number    Data
- *    Program Change               Cn        Program Number       Unused
- *    Channel Aftertouch           Dn        Pressure             Unused
- *    Pitch Wheel                  En        LSB                  MSB
- *
- * Key
- * n is the MIDI Channel Number (0-F)
- * LSB is the Least Significant Byte
- * MSB is the Least Significant Byte
- * There are several different types of controller messages.
- *
- * useful links, random order:
- *  https://en.wikipedia.org/wiki/CV/gate
- *  https://www.instructables.com/id/Send-and-Receive-MIDI-with-Arduino/
- *  http://www.songstuff.com/recording/article/midi_message_format
- *  https://espace-lab.org/activites/projets/en-arduino-processing-midi-data/
- *  https://learn.sparkfun.com/tutorials/midi-shield-hookup-guide/example-2-midi-to-control-voltage
- *  https://provideyourown.com/2011/analogwrite-convert-pwm-to-voltage/
- *  https://www.midi.org/specifications/item/table-3-control-change-messages-data-bytes-2
- *  https://arduino-info.wikispaces.com/Arduino-PWM-Frequency
- *
- *  by Barito, 2017 - 2018
- */
-#include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_MCP4725.h>
+#include <AH_MCP4921.h>
 #include <MIDI.h>
 
-bool debug = false;
+//ATmega ADC intern
+#define div_pin A5
+#define vel_pin 10
+#define cc_four 9
+#define cc_three 6
+#define cc_two 5
+#define cc_one 3
 
-#define NUMBER_OF_CC 6
-#define NUMBER_OF_GATES 5
+//digitale pins
+#define clk_pin 8
+#define gate_pin 4
 
-//set at your will ...
-#define REVERSE_GATE 0 //V-Trig = 0, S-Trig = 1
-bool HZV = 1; //set to "0" for V/oct
+//mcp4921 Pins
+#define mcp_mosi 11
+#define mcp_sck 13
+#define mcp_cs 7
 
-byte ccMax = 255;
-byte midiChannel = 1;
+//MIDI Sachen
+#define MIDI_CHANNEL 13
 
-Adafruit_MCP4725 dac;
+AH_MCP4921 AnalogOutput(mcp_mosi,mcp_sck,mcp_cs);
 
+short divide;
+short i = 0;
 
-//digital
-byte channel1gatePin = 12;
-//analog
-byte velocityPin = 5; //pwm frequency is going to be increased for this in the setup
+short CC_one = 102;
+short CC_two = 103;
+short CC_three = 104;
+short CC_four = 105;
 
-float outVoltmV;
-int velocityOut;
-int CCOut;
+long previousMillis;
+long currentMillis;
 
-unsigned long lastUpdateTime = 0;  // the last time the output pin was toggled
-unsigned long updateDelay = 500;    // the debounce time; increase if the output flickers
-
-uint16_t dacValue;
-//CHANGE IF BY PRESSING "C" ON YOUR KEYBOARD YOU HAVE ANOTHER NOTE OUTPUTTED BY THE SYNTH (HZ/V).
-int noteHZVshift = -1;//With external USB hub I had to set this to "-1". With PC USB to "+2".
-//CHANGE IF YOU HAVE A DETUNING BETWEEN ADIACENT NOTES/OCTAVES (V/oct).
-float VoctLinCoeff = 0.0790;//If your +5V are straight +5.000V this is 1/12 = 0.0833.
-//CHANGE TO SHIFT BY OCTAVES (V/oct).
-float VoctShift = -2.0;
-byte lastNote;
 MIDI_CREATE_DEFAULT_INSTANCE();
 
+void setup() {
+  pinMode(clk_pin, OUTPUT);
+  pinMode(gate_pin, OUTPUT);
 
-struct noteInfo
-{
-    byte pin;
-    byte note;
-    bool triggered;
-};
+  pinMode(cc_one, OUTPUT);
+  pinMode(cc_two, OUTPUT);
+  pinMode(cc_three, OUTPUT);
+  pinMode(cc_four, OUTPUT);
 
-struct ccInfo
-{
-    byte pin;
-    byte midiCC;
-    byte value;
-};
+  pinMode(vel_pin, OUTPUT);
 
-ccInfo avaibleCC[NUMBER_OF_CC];
-noteInfo avaibleNotes[NUMBER_OF_GATES];
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.setHandleNoteOff(handleNoteOff);
+  MIDI.setHandleControlChange(handleControlChange);
+  MIDI.setHandleClock(handleClock);
+  
+  MIDI.begin(MIDI_CHANNEL);
 
-void fillCcStruct( int index, byte pin, byte midiCC, byte value)
-{
-    avaibleCC[index].pin = pin;
-    avaibleCC[index].midiCC = midiCC;
-    avaibleCC[index].value = value;
+  //Serial.begin(31250);
+
+  //digitalWrite(mcp_cs, HIGH);
+  digitalWrite(gate_pin, LOW);
+  digitalWrite(clk_pin, LOW);
 }
 
-void fillNoteStruct(int index, byte pin, byte note, byte triggered) {
-    avaibleNotes[index].pin = pin;
-    avaibleNotes[index].note = note;
-    avaibleNotes[index].triggered = false;
+void loop() {
+
+  MIDI.read();
+
+  
+  
+  if (i == clk_div()) {
+    digitalWrite(clk_pin, HIGH);
+    previousMillis = millis();
+  }
+
+  if (millis() - previousMillis == 20) {
+    digitalWrite(clk_pin, LOW);
+    
+  }
+
+  if (i >= clk_div()) {
+    i = 0;
+  }
+
+  
 }
 
-
-void handleNoteOn(byte channel, byte note, byte velocity)
-{
-    if(channel == 1) {
-        lastNote = note;
-        //Hz/V; x 1000 because map truncates decimals
-        if (HZV) {
-            //0.125*1000
-            //V/oct; x 1000 because map truncates decimals
-            outVoltmV = 125.0*exp(0.0578*(note+noteHZVshift));
-        } else {
-            outVoltmV = 1000*((note*VoctLinCoeff)+ VoctShift);
-        }
-        dacValue = constrain(map(outVoltmV, 0, 5000, 0, 4095), 0, 4095);
-        dac.setVoltage(dacValue, false);
-        if(REVERSE_GATE == 1) {
-            digitalWrite(channel1gatePin, LOW);
-        } else {
-            digitalWrite(channel1gatePin, HIGH);
-        }
-        velocityOut = map(velocity, 0, 127, 0, 255);
-        analogWrite(velocityPin, velocityOut);
-    } else if(channel == 2) {
-        for(byte i = 0; i < NUMBER_OF_GATES; i++) {
-            if(note == avaibleNotes[i].note) {
-                digitalWrite(avaibleNotes[i].pin, HIGH);
-            }
-        }
-    }
-}
-
-void handleNoteOff(byte channel, byte note, byte velocity)
-{
-    if(channel == 1) {
-        if(note == lastNote) {
-            dac.setVoltage(0, false);
-            if(REVERSE_GATE == 1) {
-                digitalWrite(channel1gatePin, HIGH);
-            } else {
-                digitalWrite(channel1gatePin, LOW);
-            }
-            analogWrite(velocityPin, 0);
-        }
-    }else if(channel == 2){
-        for(byte i = 0; i < NUMBER_OF_GATES; i++) {
-            if(note == avaibleNotes[i].note) {
-                digitalWrite(avaibleNotes[i].pin, LOW);
-            }
-        }
-
-    }
-}
-
-void handleControlChange(byte channel, byte number, byte value)
-{
-    for(byte i = 0; i < NUMBER_OF_CC; i++) {
-        if(number == avaibleCC[i].midiCC) {
-            avaibleCC[i].value = value;
-            CCOut = map(value, 0, 127, 0, 255);
-            analogWrite(avaibleCC[i].pin, CCOut);
-        }
-    }
-}
-
-void setup()
-{
-    if(debug) {
-        Serial.print(MIDI_CHANNEL_OMNI);
-    }
-    lastUpdateTime = millis();
-    //For Arduino Uno, Nano, and any other board using ATmega 8, 168 or 328
-    TCCR0B = TCCR0B & B11111000 | B00000001;
-    //// D5, D6: set timer 0 divisor to 1 for PWM frequency of 62500.00 Hz
-    TCCR1B = (TCCR1B & B11111000) | B00000001;    // D9, D10: set timer 1 divisor to 1 for PWM frequency of 31372.55 Hz
-    //TCCR2B = TCCR2B & B11111000 | B00000001;
-    //// D3, D11: set timer 2 divisor to 1 for PWM frequency of 31372.55 Hz
-
-    fillCcStruct(0, 6, 20, 23);
-    fillCcStruct(1, 9, 21, 33);
-    fillCcStruct(2, 11, 22, 77);
-    fillCcStruct(3, 10, 23, 99);
-    fillCcStruct(4, 3, 23, 99);
-    fillCcStruct(5, 5, 23, 99);
-
-    //gates for channel 2
-    fillNoteStruct(0, 2, 0, false);
-    fillNoteStruct(1, 4, 1, false);
-    fillNoteStruct(2, 7, 2, false);
-    fillNoteStruct(3, 8, 3, false);
-    //setup pins
-    pinMode(channel1gatePin, OUTPUT);
-    pinMode(velocityPin, OUTPUT);
-    for(byte i = 0; i < NUMBER_OF_CC; i++) {
-        pinMode(avaibleCC[i].pin, OUTPUT);
-    }
-
-    for(byte i = 0; i < NUMBER_OF_GATES; i++) {
-        pinMode(avaibleNotes[i].pin, OUTPUT);
-    }
-
-    if(REVERSE_GATE == 1){
-        //S-Trig
-        digitalWrite(channel1gatePin, HIGH);
-    } else {
-        //V-Trig
-        digitalWrite(channel1gatePin, LOW);
-    }
-
-    ////setup MIDI handles
-    MIDI.setHandleNoteOn(handleNoteOn);
-    MIDI.setHandleNoteOff(handleNoteOff);
-    MIDI.setHandleControlChange(handleControlChange);
-
-    //// start MIDI and listen to channel "midiChannel"
-    if(debug) {
-        Serial.begin(9600);
-    } else {
-        MIDI.begin(MIDI_CHANNEL_OMNI);
-    }
-    //// For Adafruit MCP4725A1 the address is 0x62 (default) or 0x63 (ADDR pin tied to VCC)
-    //// For MCP4725A0 the address is 0x60 or 0x61
-    //// For MCP4725A2 the address is 0x64 or 0x65
-    dac.begin(0x62);
+void handleClock(void){
+  i++;
 
 }
 
-void loop()
-{
-    MIDI.read(MIDI_CHANNEL_OMNI);
-}
+void  handleNoteOn(byte channel, byte pitch, byte velocity) {
+ 
+    digitalWrite(gate_pin, HIGH); 
+
+    //update MCP4921
+    const float VOctLinCoeff = 0.0833; //Aref vom DAC ist genau 5V -> (1/12)*5V = 0,0833
+    //const float note = map(pitch, 0, 127, 0, 4095);
+    const float VOctShift = -1.75; //DAC ist auf A0 bis A5 gestimmt!
+    
+    float cv_out = 1000*(VOctLinCoeff*pitch+VOctShift); // funktion in der Klammer gibt werte von 0 - 5 raus
+                                                        // *1000 weil map funktion die kommastellen abschneidet
+    unsigned int dac_out = constrain(map(cv_out, 0, 5000, 0, 4095), 0, 4095);  //map(cv_out, 0, 5000, 0, 4095)
+    
+    AnalogOutput.setValue(dac_out);
+
+    //update velocity pin
+    short velOut = map(velocity, 0, 127, 0, 255);
+    analogWrite(vel_pin, velOut); 
+  //check MIDI channel
+}//handleNoteOn
+
+void handleNoteOff(byte channel, byte pitch, byte velocity) {
+  
+    digitalWrite(gate_pin, LOW);
+
+    //update velocity
+    analogWrite(vel_pin, 0);
+  
+}//handleNoteOff
+
+void handleControlChange (byte channel, byte number, byte value) {
+  if(number == CC_one) {
+    int ccOneOut = map(value, 0,127, 0, 255);
+    analogWrite(cc_one, ccOneOut);
+  }//CC_one
+
+  if (number == CC_two){
+    int ccTwoOut = map (value, 0, 127, 0, 255);
+    analogWrite(cc_two, ccTwoOut);
+  }//CC_two
+
+  if(number == CC_three){
+    int ccThreeOut = map(value, 0, 127, 0, 255);
+    analogWrite(cc_three, ccThreeOut);
+  }//CC_three
+
+  if(number == CC_four){
+    int ccFourOut = map(value, 0, 127, 0, 255);
+    analogWrite(cc_four, ccFourOut);
+  }//CC_four
+}//handleControlChange
+
+short clk_div() {
+  int adc_val = analogRead(div_pin);
+
+  divide = adc_val >> 7;
+
+  switch(divide){
+    case 0:
+      return 3;
+      break;
+    case 1:
+      return 6;
+      break;
+    case 2:
+      return 12;
+      break;
+    case 3:
+      return 24;
+      break;
+    case 4:
+      return 24; 
+      break;
+    case 5:
+      return 48;
+      break;
+    case 6:
+      return 48;
+      break;
+    case 7:
+      return 96;
+      break;
+  }
+}//clk_div
